5cf69211fe846a586b4e651e76997b22c664b43d
DevMatheusMoura <matheussilvamouras@gmail.com>
2025-04-17 19:33:51 -0300

    implementa camada service

--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.validation.constraints.Email;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -41,4 +43,30 @@ public class Usuario {
 		this.status = StatusUsuario.FOCO;
 		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
 	}
+
+    public void mudaStatusParaFoco(UUID idUsuario) {
+		perteceAoUsuario(idUsuario);
+		verificaStatusFoco();
+		alteraStatusFoco();
+    }
+
+	private void alteraStatusFoco() {
+		this.status = StatusUsuario.FOCO;
+	}
+
+	private void verificaStatusFoco() {
+		if (this.status.equals(StatusUsuario.FOCO)) {
+			throw APIException.build(HttpStatus.CONFLICT, "O usuário já está em foco.");
+		}
+
+	}
+
+	private void perteceAoUsuario(UUID usuarioFoco) {
+		if (!this.idUsuario.equals(usuarioFoco)) {
+			throw APIException.build(HttpStatus.UNAUTHORIZED, " credencial de autenticação não é válida. ");
+		}
+	}
+
+
 }
+
