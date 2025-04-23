8b989b251b9dc69fb445968c5f72c67ff0f83393
Ronan Lima <ronanlimasilva@hotmail.com>
2025-04-22 21:18:40 -0300

    feat: implementa camada API

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -17,4 +17,9 @@ public interface UsuarioAPI {
 	@GetMapping(value = "/{idUsuario}")
 	@ResponseStatus(code = HttpStatus.OK)
 	UsuarioCriadoResponse buscaUsuarioPorId(@PathVariable UUID idUsuario);
+
+	@PatchMapping("/pausaLonga/{idUsuario}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void mudaStatusPausaLonga(@RequestHeader(name = "Authorization", required = true) String token,
+							  @PathVariable UUID idUsuario);
 }
