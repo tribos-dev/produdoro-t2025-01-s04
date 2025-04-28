be208f24df567ce6c1d7cd7116184fac2e820140
zeltolira <zelto16@hotmail.com>
2025-04-28 14:02:07 -0300

    test: implementa testes unitarios

--- src/test/java/dev/wakandaacademy/produdoro/DataHelper.java
+++ src/test/java/dev/wakandaacademy/produdoro/DataHelper.java
@@ -19,6 +19,9 @@ public class DataHelper {
     public static Usuario createUsuario() {
         return Usuario.builder().email("email@email.com").status(StatusUsuario.PAUSA_LONGA).idUsuario(usuario1).build();
     }
+    public static Usuario createUsuarioFoco() {
+        return Usuario.builder().email("email@email.com").status(StatusUsuario.FOCO).idUsuario(usuario1).build();
+    }
 
     public static Usuario createUsuario2() {
         return Usuario.builder().email("email@email.com").status(StatusUsuario.PAUSA_CURTA).idUsuario(usuario1).build();
